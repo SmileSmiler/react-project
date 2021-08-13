0c8e2ebfefdc5ca5cfca51fe8697af547ae6d2e8
AOBI\abdilmanov_y <abdilmanov_y@bi.group>
2021-08-13 12:28:53 +0600

    1.01

--- src/App.js
+++ src/App.js
@@ -3,15 +3,26 @@ import './App.css';
 
 const App = () => {
   return (
-    <div>
+    <div className="app-wrapper">
       <header>
         <img src="https://upload.wikimedia.org/wikipedia/ru/thumb/7/7a/Manchester_United_FC_crest.svg/1010px-Manchester_United_FC_crest.svg.png"></img>
       </header>
       <nav>
-        <div></div>
+        <div>
+          <a>first</a>
+        </div>
+        <div>
+          <a>second</a>
+        </div>
+        <div>
+          <a>third</a>
+        </div>
+        <div>
+          <a>four</a>
+        </div>
       </nav>
       <div>
-
+        content
       </div>
     </div>
   );
