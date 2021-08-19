456eee51d85b93bc3696d6212ac1d125a87d6eee
AOBI\abdilmanov_y <abdilmanov_y@bi.group>
2021-08-19 18:32:36 +0600

    dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import s from './Dialogs.module.css'
+import { NavLink } from "react-router-dom"
 
-const Dialogs = () => {
+const Chek = () => {
   return (
-    <div>
-      Dialogs
+    <div className={s.dialogs}>
+      <div className={s.dialogsItems}>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/1" activeClassName={s.active}>name1</NavLink>
+        </div>
+        <div className={s.dialog}>
+          <NavLink to="/dialogs/2" activeClassName={s.active}>name2</NavLink>
+        </div>
+      </div>
+      <div>
+        <div className={s.messages}>
+          <div className={s.message}>
+            text text
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
 
-export default Dialogs
+export default Chek
